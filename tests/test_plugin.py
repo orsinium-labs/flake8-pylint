168fae95ef678f75e12deb752f767c86dc856dba
gram <git@orsinium.dev>
2023-03-04 08:41:46 +0100

    add a smoke test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,5 +1,29 @@
 from __future__ import annotations
 
+import subprocess
+import sys
+from pathlib import Path
+from textwrap import dedent
 
-def test_smoke():
-    pass
+
+def test_smoke(tmp_path: Path) -> None:
+    file_path = tmp_path / 'example.py'
+    source = """
+        def f():
+            pass
+    """
+    file_path.write_text(dedent(source))
+    cmd = [sys.executable, '-m', 'flake8', '--isolated', str(tmp_path)]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+
+    assert result.returncode == 1
+    msgs = result.stdout.decode().strip().splitlines()
+    print(*msgs, sep='\n')
+
+    assert len(msgs) == 3
+    for msg in msgs:
+        assert 'example.py:' in msg
+        assert ':1: PLC0' in msg
+    assert 'PLC0114 Missing module docstring (missing-module-docstring)' in msgs[0]
+    assert 'PLC0116 Missing function or method docstring' in msgs[1]
+    assert 'PLC0103 Function name "f"' in msgs[2]
